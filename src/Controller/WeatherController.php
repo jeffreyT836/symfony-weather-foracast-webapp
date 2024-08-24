682ff2fa46945f366229e7a8b47ff837ccab8aa8
Jeffrey Tempelman <jeffrey.tempelman@expedition.nl>
2024-08-24 18:18:07 +0200

    [WFA] feat: move routing to routes.yaml

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -3,20 +3,19 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-    class WeatherController extends AbstractController
+class WeatherController
 {
-    #[Route('/weather/highlander-says')]
+    #[Route('/weather/highlander-says', name: 'weather highlander-says')]
     public function highlanderSays(): Response
     {
         $randomNumber = random_int(1, 100);
-        $forecast = $randomNumber < 50 ? 'It\'s going to rain' : 'It\'s going to be sunny';
+        $weather = $randomNumber < 50 ? 'It\'s going to rain' : 'It\'s going to be sunny';
 
-        return $this->render('weather/highlander_says.html.twig', [
-            'forecast' => $forecast,
-        ]);
+        return new Response(
+            "<html><body><h1>{$weather}</h1></body></html>"
+        );
     }
 }
