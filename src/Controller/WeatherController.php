fbbf0927a84e75da407082581cd911cf0c2bd2f9
Jeffrey Tempelman <jeffrey.tempelman@expedition.nl>
2024-08-24 18:25:04 +0200

    [WFA] fix: fix merge conflicts

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -3,19 +3,20 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class WeatherController
+class WeatherController extends AbstractController
 {
     #[Route('/weather/highlander-says', name: 'weather highlander-says')]
     public function highlanderSays(): Response
     {
         $randomNumber = random_int(1, 100);
-        $weather = $randomNumber < 50 ? 'It\'s going to rain' : 'It\'s going to be sunny';
+        $forecast = $randomNumber < 50 ? 'It\'s going to rain' : 'It\'s going to be sunny';
 
-        return new Response(
-            "<html><body><h1>{$weather}</h1></body></html>"
-        );
+        return $this->render('weather/highlander_says.html.twig', [
+            'forecast' => $forecast
+        ]);
     }
 }
